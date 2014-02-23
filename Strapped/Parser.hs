60e910ba5136ff373d593122a438e65d6efd6f3c
Kyle Hanson <kyle@esharesinc.com>
2014-02-22 22:12:03 -0800

    Benchmark

--- Strapped/Parser.hs
+++ Strapped/Parser.hs
@@ -2,6 +2,10 @@ module Strapped.Parser where
 
 import Control.Applicative ((<*>))
 import Control.Monad
+import Data.Monoid
+import qualified Data.Text as T
+import Blaze.ByteString.Builder as B
+import Blaze.ByteString.Builder.Char.Utf8 as B
 import Text.ParserCombinators.Parsec
 
 import Strapped.Types
@@ -12,7 +16,7 @@ peekChar = void . try . lookAhead . char
 
 tryTag = void . try . tag
 
-tag p = between (string "{$" >> spaces) (spaces >> string "$}") p <?> "Tag"
+tag p = between (string "{@" >> spaces) (spaces >> string "@}") p <?> "Tag"
 
 parseContent end = do
   ps <- manyTill parsePiece end
@@ -34,16 +38,26 @@ parseFor = do
         l <- wordString
         return (v, l)
 
+parseFunc = do
+  (var, args) <- between (string "@{" >> spaces) (spaces >> string "}") argParse <?> "Variable tag"
+  return $ FuncPiece var args
+  where argParse = do
+          s <- wordString
+          spaces
+          s1 <- wordString
+          others <- wordString `sepBy` (string " ")
+          return $ (s, s1:others)
+
 parseVar = do
-  var <- between (string "${" >> spaces) (spaces >> string "}") wordString <?> "Variable tag"
+  var <- between (string "@{" >> spaces) (spaces >> string "}") wordString <?> "Variable tag"
   return $ VarPiece var
 
 parseStatic = do
   c <- anyChar
-  s <- manyTill anyChar (peekChar '{' <|> peekChar '$' <|> eof)
-  return $ StaticPiece (c:s)
+  s <- manyTill anyChar (peekChar '{' <|> peekChar '@' <|> eof)
+  return $ StaticPiece (packInput $ c:s)
 
-parseNonStatic = try parseBlock <|> try parseFor <|> parseVar
+parseNonStatic = try parseBlock <|> try parseFor <|> try parseFunc <|> parseVar
 parsePiece = try parseNonStatic <|> parseStatic
 
 parsePieces = parseContent eof
@@ -55,7 +69,7 @@ isStatic :: Piece -> Bool
 isStatic (StaticPiece _) = True
 isStatic _ = False
 
-unStatic :: Piece -> String
+unStatic :: Piece -> Output
 unStatic (StaticPiece s) = s
 unStatic _ = error "unStatic: applied to something other than a StaticPiece"
 
@@ -64,7 +78,8 @@ combineStatics pieces = let (nonstatics,therest) = span (not.isStatic) pieces in
     nonstatics ++ combine therest where
       combine [] = []
       combine ps = let (statics,more) = span isStatic ps in
-        (StaticPiece . concat . map unStatic) statics : combineStatics more
+        (StaticPiece . mconcat . map unStatic) statics : combineStatics more
 
-parseTemplate :: String -> Either ParseError [Piece]
-parseTemplate = parse parsePieces ""
+parseTemplate :: String -> Either ParseError Template
+parseTemplate s = fmap mkTemplate $ parse parsePieces "" s
+  where mkTemplate c = Template c []
--- Strapped/Types.hs
+++ Strapped/Types.hs
@@ -1,7 +1,32 @@
 module Strapped.Types where
 
-data Piece = StaticPiece String 
+import Blaze.ByteString.Builder
+import Blaze.ByteString.Builder.Char8
+import Control.Monad.Error
+import Data.Text as T
+
+packInput = fromString
+
+type Output = Builder
+
+data Piece = StaticPiece Output
            | BlockPiece String [Piece]
-           | VarPiece String
            | ForPiece String String [Piece]
+           | VarPiece String
+           | FuncPiece String [String]
+
+data Input m = 
+             Value Output 
+           | List [Input m]
+           | Func  ([Input m] -> ErrorT StrapError m Output)  
+
+data StrapError = StrapError String | InputNotFound String
   deriving (Show)
+  
+instance Error StrapError where
+  noMsg    = StrapError "A Strap Error"
+  strMsg s = StrapError s
+
+type InputGetter m = String -> Maybe (Input m)
+
+data Template = Template [Piece] [Piece]
